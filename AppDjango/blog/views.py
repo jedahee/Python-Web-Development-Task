b1507deda495110717e9684b0cb2f8e147acff44
Jesus <jedahee02@gmail.com>
2022-02-15 09:35:53 +0100

    Seguir app casas moodle

--- AppDjango/blog/views.py
+++ AppDjango/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import General
 
 # Create your views here.
+def home(request):
+    general = General.objects.first()
+    
+    return render(request, 'blog/home.html', {
+        general: 'general'
+    })
