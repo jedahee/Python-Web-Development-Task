b929a6eb85c3fe64707f8a8da1745e217ad31b24
Jesus <jedahee02@gmail.com>
2022-02-18 13:04:46 +0100

    Seguir pdf

--- AppDjango/blog/models.py
+++ AppDjango/blog/models.py
@@ -8,3 +8,36 @@ class General(models.Model):
     
     def __str__(self):
         return self.name
+
+# Users
+class User(models.Model):
+    name = models.CharField(max_length=100, unique=True, null=False)
+    email = models.CharField(max_length=100, null=False)
+    password = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.name
+
+# Posts
+class Post(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    slug = models.CharField(max_length=100, null=False)
+    img = models.CharField(max_length=60)
+    content = models.TextField(null=False)
+    published = models.BooleanField(False)
+    outstanding = models.BooleanField(False)
+    creation_date = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+# Categories
+class Categories(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    slug = models.CharField(max_length=100, null=False)
+
+# Tags
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    slug = models.CharField(max_length=100, null=False)
+    description = models.TextField()
